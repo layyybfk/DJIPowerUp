98411c01ae023a64e674069446b40336a57e1849
Layne <layne.lu@buaa.edu.cn>
2019-01-06 22:54:22 +0800

    day3 新建一个工程

---
+++ day3/MDKPro/Objects/stm32h7xx_hal_smartcard.d
@@ -0,0 +1,35 @@
+./objects/stm32h7xx_hal_smartcard.o: \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Src\stm32h7xx_hal_smartcard.c \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal.h \
+  RTE\Device\STM32H743VITx\STCubeGenerated\Inc\stm32h7xx_hal_conf.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_rcc.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_def.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\CMSIS\Device\ST\STM32H7xx\Include\stm32h7xx.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\CMSIS\Device\ST\STM32H7xx\Include\stm32h743xx.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\core_cm7.h \
+  C:\Software\Keil\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Software\Keil\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Software\Keil\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\CMSIS\Device\ST\STM32H7xx\Include\system_stm32h7xx.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  C:\Software\Keil\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_rcc_ex.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_gpio.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_gpio_ex.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_dma.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_dma_ex.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_cortex.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_flash.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_flash_ex.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_hsem.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_i2c.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_i2c_ex.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_mdma.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_pwr.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_pwr_ex.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_uart.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\STM32H7xx_HAL_Driver\Inc\stm32h7xx_hal_uart_ex.h
---
+++ day3/MDKPro/Objects/system_stm32h7xx.d
@@ -0,0 +1,12 @@
+./objects/system_stm32h7xx.o: RTE\Device\STM32H743VITx\system_stm32h7xx.c \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\CMSIS\Device\ST\STM32H7xx\Include\stm32h7xx.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\CMSIS\Device\ST\STM32H7xx\Include\stm32h743xx.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\core_cm7.h \
+  C:\Software\Keil\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Software\Keil\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Software\Keil\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Software\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Software\Keil\ARM\PACK\Keil\STM32H7xx_DFP\2.2.0\Drivers\CMSIS\Device\ST\STM32H7xx\Include\system_stm32h7xx.h
